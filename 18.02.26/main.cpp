bd3e90fb82afac22ca93c3ad6d4abc73cb601b3b
SenziFox <arseniitrs@gmail.com>
2026-02-18 15:54:47 +0300

    inital

---
+++ 18.02.26/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class point2D {
+public:
+    point2D() {
+        _x = 0;
+        _y = 0;
+    }
+
+    point2D(int x, int y) {
+        _x = x;
+        _y = y;
+    }
+
+    void setX(int x) {
+        _x = x;
+    }
+
+    int getX() {
+        return _x;
+    }
+
+    void setY(int y) {
+        _y = y;
+    }
+
+    int getY() {
+        return _y;
+    }
+
+    void showPoint() {
+        cout << "my Point" << endl;
+        cout << "x: " << _x << endl;
+        cout << "y: " << _y << endl;
+    }
+
+private:
+    int _x;
+    int _y;
+};
+
+int main() {
+    int x, y;
+    point2D myPoint1;
+
+    cout << "Enter x and y: " << endl;
+
+
+    myPoint1.setX(x);
+    myPoint1.setY(y);
+    myPoint1.showPoint();
+    myPoint1.setX(99);
+    myPoint1.setY(333);
+    myPoint1.showPoint();
+    cout << " X = " << myPoint1.getX() << endl;
+    cout << " Y = " << myPoint1.getY() << endl;
+    return 0;
+}
